c49d16c4fadbefc6af23864d0186a1631ee30b07
CarlosAlvia <cjalvia@espol.edu.ec>
2023-11-17 10:26:03 -0500

    Limpieza del código

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -5,6 +5,7 @@ import {
   temperaturaArr,
 } from "./static_data.js";
 let fechaActual = () => new Date().toISOString().slice(0, 10);
+
 let cargarPrecipitacion = () => {
   //Obtenga la función fechaActual
   let actual = fechaActual();
@@ -32,11 +33,11 @@ let cargarPrecipitacion = () => {
   let precipitacionMaxValue = document.getElementById("precipitacionMaxValue");
   //Actualice los elementos HTML con los valores correspondientes
   precipitacionMinValue.textContent = `Min ${min} [mm]`;
-  precipitacionPromValue.textContent = `Prom ${
-    Math.round(prom * 100) / 100
-  } [mm]`;
+  precipitacionPromValue.textContent = `Prom ${Math.round(prom * 100) / 100
+    } [mm]`;
   precipitacionMaxValue.textContent = `Max ${max} [mm]`;
 };
+
 let cargarFechaActual = () => {
   //Obtenga la referencia al elemento h6
   let coleccionHTML = document.getElementsByTagName("h6");
@@ -45,76 +46,149 @@ let cargarFechaActual = () => {
   //Actualice la referencia al elemento h6 con el valor de la función fechaActual()
   tituloH6.textContent = fechaActual();
 };
+
+let crearConfig = (tipoGrafico, lbl1, lblDataset, data) => {
+  let config = {
+    type: tipoGrafico,
+    data: {
+      labels: lbl1,
+      datasets: [
+        {
+          label: lblDataset,
+          data: data,
+        },
+      ],
+    },
+  };
+  return config;
+}
 let cargarOpenMeteo = () => {
   //URL que responde con la respuesta a cargar
   let URL =
-    "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto";
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,weather_code&timezone=auto";
 
   fetch(URL)
     .then((responseText) => responseText.json())
     .then((responseJSON) => {
-      console.log(responseJSON);
       //Respuesta en formato JSON
       let plotRef = document.getElementById("plot1");
-
+      let plotRef2 = document.getElementById("plot2");
       //Etiquetas del gráfico
       let labels = responseJSON.hourly.time;
 
       //Etiquetas de los datos
       let data = responseJSON.hourly.temperature_2m;
-      let config = {
-        type: "line",
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: "Temperature [2m]",
-              data: data,
-            },
-          ],
-        },
-      };
+      let config = crearConfig("line", labels, "Temperature [2m]", data);
+
+      let data2 = responseJSON.hourly.precipitation_probability;
+      let config2 = crearConfig("line", labels, "Precipitation probability", data2);
+
       let chart1 = new Chart(plotRef, config);
+      let chart2 = new Chart(plotRef2, config2);
     })
     .catch(console.error);
-
-
-
-
 };
-//ARREGLAR PARA HACER AMBOS GRÁFICOS CON UNA SOLA FUNCIÓN
-let cargarOpenMeteo2 = () => {
-  let URL2 =
-  "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=precipitation_probability&timezone=auto";
-  fetch(URL2)
-    .then((responseText) => responseText.json())
-    .then((responseJSON) => {
-      let plotRef2 = document.getElementById("plot2");
 
-      //Etiquetas del gráfico
-      let labels = responseJSON.hourly.time;
-
-      //Etiquetas de los datos
-      let data = responseJSON.hourly.precipitation_probability;
-      let config2 = {
-        type: "line",
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: "Precipitation probability",
-              data: data,
-            },
-          ],
-        },
-
-        //Respuesta en formato JSO
-      };
-      let chart2 = new Chart(plotRef2, config2);
-    });
-
+let parseXML = (responseText) => {
+  // Parsing XML
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+  return xml;
 };
+
+let renderizarPlantilla = (xml) =>{
+  // Referencia al elemento `#forecastbody` del documento HTML
+  let forecastElement = document.querySelector("#forecastbody");
+  forecastElement.innerHTML = "";
+
+  // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+  let timeArr = xml.querySelectorAll("time");
+  timeArr.forEach((time) => {
+    let template = procesarTimeConPlantilla(time);
+    //Renderizando la plantilla en el elemento HTML
+    forecastElement.innerHTML += template;
+  });
+}
+
+let procesarTimeConPlantilla = (time) => {
+  let from = time.getAttribute("from").replace("T", " ");
+  let humidity = time.querySelector("humidity").getAttribute("value");
+  let windSpeed = time.querySelector("windSpeed").getAttribute("mps");
+  let precipitation = time.querySelector("precipitation").getAttribute("probability");
+  let pressure = time.querySelector("pressure").getAttribute("value");
+  let cloud = time.querySelector("clouds").getAttribute("all");
+  return llenarPlantilla(from, humidity, windSpeed, precipitation, pressure, cloud);
+}
+
+let llenarPlantilla = (from, humidity, windSpeed, precipitation, pressure, cloud) => {
+  let template = `
+  <tr>
+      <td>${from}</td>
+      <td>${humidity}</td>
+      <td>${windSpeed}</td>
+      <td>${precipitation}</td>
+      <td>${pressure}</td>
+      <td>${cloud}</td>
+  </tr>
+  `;
+  return template;
+}
+
+//Callback
+let selectListener = async (event) => {
+  let selectedCity = event.target.value;
+  // Lea la entrada de almacenamiento local
+  let cityStorage = localStorage.getItem(selectedCity);
+
+  if (cityStorage == null) {
+    cargarInfoApi(selectedCity);
+  } else {
+
+    // Procese un valor previo
+    renderizarPlantilla(parseXML(cityStorage));
+  }
+  console.log(selectedCity);
+};
+
+let cargarInfoApi = async (selectedCity) => {
+  try {
+    //API key
+    let APIkey = "44457df64ddd105950bfee6608efb1d2";
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`;
+
+    let response = await fetch(url);
+    let responseText = await response.text();
+
+    await renderizarPlantilla(parseXML(responseText));
+    // Guarde la entrada de almacenamiento local
+    await localStorage.setItem(selectedCity, responseText);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let loadForecastByCity = () => {
+  //Handling event
+  let selectElement = document.querySelector("select");
+  selectElement.addEventListener("change", selectListener);
+};
+
+
+let loadExternalTable = async () => {
+  //Requerimiento asíncrono
+  let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+  let endpoint = proxyURL + 'https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/'
+  let response = await fetch(endpoint);
+  let responseText = await response.text();
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(responseText, 'text/html');
+  let table = doc.querySelector('#postcontent table');
+  let monitoreo = document.getElementById('monitoreo');
+  monitoreo.innerHTML = table.outerHTML
+};
+
 cargarFechaActual();
 cargarPrecipitacion();
 cargarOpenMeteo();
-cargarOpenMeteo2();
+loadForecastByCity();
+loadExternalTable();
