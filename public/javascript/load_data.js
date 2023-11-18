d7415f752a6a43624f4f4bac6ea74f538c161fb5
CarlosAlvia <cjalvia@espol.edu.ec>
2023-11-17 21:47:33 -0500

    Cambios

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -1,51 +1,20 @@
-import {
-  tiempoArr,
-  precipitacionArr,
-  uvArr,
-  temperaturaArr,
-} from "./static_data.js";
-let fechaActual = () => new Date().toISOString().slice(0, 10);
+let obtenerHoraLocal = () => {
+  var fechaHora = new Date();
+  var horas = fechaHora.getHours();
+  var minutos = fechaHora.getMinutes();
+  minutos = minutos < 10 ? '0' + minutos : minutos;
+  var horaLocal = horas + ':' + minutos;
+  let elementoHora = document.getElementById("hora");
+  elementoHora.innerHTML=horaLocal;
+}
 
-let cargarPrecipitacion = () => {
-  //Obtenga la función fechaActual
-  let actual = fechaActual();
-  //Defina un arreglo temporal vacío
-  let datos = [];
-  //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
-  for (let index = 0; index < tiempoArr.length; index++) {
-    const tiempo = tiempoArr[index];
-    const precipitacion = precipitacionArr[index];
+setTimeout(() => {
+  setInterval(() => {
+    obtenerHoraLocal();
+  }, 60000);
+  obtenerHoraLocal();
+}, (60 - new Date().getSeconds()) * 1000);
 
-    if (tiempo.includes(actual)) {
-      datos.push(precipitacion);
-    }
-  }
-  //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
-  let max = Math.max(...datos);
-  let min = Math.min(...datos);
-  let sum = datos.reduce((a, b) => a + b, 0);
-  let prom = sum / datos.length || 0;
-  //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
-  let precipitacionMinValue = document.getElementById("precipitacionMinValue");
-  let precipitacionPromValue = document.getElementById(
-    "precipitacionPromValue"
-  );
-  let precipitacionMaxValue = document.getElementById("precipitacionMaxValue");
-  //Actualice los elementos HTML con los valores correspondientes
-  precipitacionMinValue.textContent = `Min ${min} [mm]`;
-  precipitacionPromValue.textContent = `Prom ${Math.round(prom * 100) / 100
-    } [mm]`;
-  precipitacionMaxValue.textContent = `Max ${max} [mm]`;
-};
-
-let cargarFechaActual = () => {
-  //Obtenga la referencia al elemento h6
-  let coleccionHTML = document.getElementsByTagName("h6");
-
-  let tituloH6 = coleccionHTML[0];
-  //Actualice la referencia al elemento h6 con el valor de la función fechaActual()
-  tituloH6.textContent = fechaActual();
-};
 
 let crearConfig = (tipoGrafico, lbl1, lblDataset, data) => {
   let config = {
@@ -59,8 +28,51 @@ let crearConfig = (tipoGrafico, lbl1, lblDataset, data) => {
         },
       ],
     },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: 100
+        }
+      }
+    }
   };
   return config;
+}
+let crearConfigMaxMin = (tipoGrafico, lbl1, lblMax,dataMax, lblMin, dataMin) => {
+  let config = {
+    type: tipoGrafico,
+    data: {
+      labels: lbl1,
+      datasets: [
+        {
+          label: lblMax,
+          data: dataMax,
+        },
+        {
+          label: lblMin,
+          data: dataMin,
+        }
+      ],
+    },
+  };
+  return config;
+}
+
+// 17/11/2023, 21:00:53
+let cargarValoresActuales = (horas,temperatura,precipitaciones) => {
+  let indice = horas.indexOf(obtenerHoraFormateada());
+  document.getElementById("precipitacionValue").innerHTML = precipitaciones[indice].toString()+"%";
+  document.getElementById("temperaturaValue").innerHTML = Math.round(temperatura[indice]).toString()+"°C";
+}
+
+let obtenerHoraFormateada = () =>{
+  let fechaHora = new Date().toLocaleString().split(",");
+  let ddmmaa = fechaHora[0].split("/");
+  let hora = fechaHora[1].trim().split(":");
+  let horaABuscar= ddmmaa[2]+"-"+ddmmaa[1]+"-"+ddmmaa[0]+"T"+hora[0]+":00";
+  console.log(horaABuscar);
+  return horaABuscar;
 }
 let cargarOpenMeteo = () => {
   //URL que responde con la respuesta a cargar
@@ -70,18 +82,35 @@ let cargarOpenMeteo = () => {
   fetch(URL)
     .then((responseText) => responseText.json())
     .then((responseJSON) => {
-      //Respuesta en formato JSON
+
       let plotRef = document.getElementById("plot1");
       let plotRef2 = document.getElementById("plot2");
-      //Etiquetas del gráfico
-      let labels = responseJSON.hourly.time;
 
-      //Etiquetas de los datos
-      let data = responseJSON.hourly.temperature_2m;
-      let config = crearConfig("line", labels, "Temperature [2m]", data);
+    
+      let horas = responseJSON.hourly.time;
+      let temperatura = responseJSON.hourly.temperature_2m;
+      let precipitaciones = responseJSON.hourly.precipitation_probability;
 
-      let data2 = responseJSON.hourly.precipitation_probability;
-      let config2 = crearConfig("line", labels, "Precipitation probability", data2);
+      let dias = [];
+      let tempsMin = [];
+      let tempsMax = [];
+      let precipitacionMax = [];
+
+      cargarValoresActuales(horas,temperatura,precipitaciones);
+
+      for (let i = 0; i <= 6; i++) {
+        dias.push(horas[0+(24*i)].split("T")[0]);
+        let tempxDiaAscendente = temperatura.slice(0+(24*i),24+(24*i)).sort();
+        let precipitacionesxDiaAscendente = precipitaciones.slice(0+(24*i),24+(24*i)).sort();
+        tempsMin.push(tempxDiaAscendente[0]);
+        tempsMax.push(tempxDiaAscendente[23]);
+        precipitacionMax.push(precipitacionesxDiaAscendente[23]);
+      }
+
+      const temperaturasMaximas = tempsMax.map(temp => parseFloat(temp));
+      const temperaturasMinimas = tempsMin.map(temp => parseFloat(temp));
+      let config = crearConfigMaxMin("line", dias, "Temperatura Máxima", temperaturasMaximas,"Temperatura Mínima",temperaturasMinimas);
+      let config2 = crearConfig("bar", dias, "Probabilidad de precipitación %", precipitacionMax);
 
       let chart1 = new Chart(plotRef, config);
       let chart2 = new Chart(plotRef2, config2);
@@ -163,7 +192,7 @@ let cargarInfoApi = async (selectedCity) => {
     // Guarde la entrada de almacenamiento local
     await localStorage.setItem(selectedCity, responseText);
   } catch (error) {
-    console.log(error);
+
   }
 };
 
@@ -187,8 +216,9 @@ let loadExternalTable = async () => {
   monitoreo.innerHTML = table.outerHTML
 };
 
-cargarFechaActual();
-cargarPrecipitacion();
+//cargarFechaActual();
+//cargarPrecipitacion();
 cargarOpenMeteo();
 loadForecastByCity();
 loadExternalTable();
+obtenerHoraLocal();
